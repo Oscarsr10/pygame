f618034cd9df55a93dcd53a076c49ded1548f8e9
OscarSerrano <oskiitarsr@gmail.com>
2016-10-21 08:35:57 +0200

    Creacion de ventana

---
+++ juego.py
@@ -0,0 +1,19 @@
+import sys, pygame
+from pygame.locals import *
+
+
+WIDTH = 640
+HEIGHT = 480
+ 
+def main():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Pruebas Pygame")
+    while True:
+        for eventos in pygame.event.get():
+            if eventos.type == QUIT:
+                sys.exit(0)
+    return 0
+ 
+if __name__ == '__main__':
+    pygame.init()
+    main()
