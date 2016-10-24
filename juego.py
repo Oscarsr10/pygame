fc676f80c463cef7f13ff945b36de6e19a2a0b99
OscarSerrano <oskiitarsr@gmail.com>
2016-10-24 22:41:22 +0200

    Menú

--- juego.py
+++ juego.py
@@ -1,6 +1,8 @@
 import pygame
+from pygame.locals import *
 import time
 import random
+import sys
 
 pygame.init()
 
@@ -10,11 +12,15 @@ pygame.display.set_caption("Pygame")
 
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0)
+red = (200,0,0)
+green = (0,128,0)
+bright_red = (255,0,0)
+bright_green = (0,255,0)
 
 car_width = 73
 clock = pygame.time.Clock() 
 carImg = pygame.image.load('racecar.png')
+
 
 def things_dodged(count):
     font = pygame.font.SysFont(None, 25)
@@ -47,6 +53,61 @@ def message_display(text):
 
 def crash():
     message_display('You Crashed')
+    
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill(white)
+        largeText = pygame.font.SysFont('comicsansms',115)
+        TextSurf, TextRect = text_objects("Race Dodge", largeText)
+        TextRect.center = ((800/2),(600/2))
+        screen.blit(TextSurf, TextRect)
+        
+ #       mouse = pygame.mouse.get_pos()
+        
+#        if 150+100 > mouse[0] > 150 and 450+50 > mouse[1] > 450:
+#            pygame.draw.rect(screen, bright_green,(150,450,100,50))
+#        else:
+#            pygame.draw.rect(screen, green,(150,450,100,50))
+#        smallText = pygame.font.Font("freesansbold.ttf",20)
+#        textSurf, textRect = text_objects("START!", smallText)
+#        textRect.center = ( (150+(100/2)), (450+(50/2)) )
+#        screen.blit(textSurf, textRect)
+        
+#        pygame.draw.rect(screen, red,(550,450,100,50))
+        button("GO!",150,450,100,50,green,bright_green,game_loop)
+        button("Quit",550,450,100,50,red,bright_red,quitgame)
+        pygame.display.update()
+        clock.tick(15)
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(screen, ac,(x,y,w,h))
+        
+        if click[0] == 1 and action != None:
+            action()  
+    else:
+        pygame.draw.rect(screen, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    screen.blit(textSurf, textRect)
+    
+def quitgame():
+    pygame.quit()
+    quit()
 
 def game_loop():
     x =  (800 * 0.45)
@@ -94,18 +155,20 @@ def game_loop():
             thing_startx = random.randrange(0,800) 
             dodged += 1
             thing_speed += 1
-            thing_width += (dodged * 1.0)
+            thing_width += (dodged * 0.8)
         
         if y < thing_starty+thing_height:
             print('y crossover')
 
             if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
                 print('x crossover')
-                crash()     
+                crash()
+    
 
         pygame.display.update()
         clock.tick(60)
 
+game_intro()
 game_loop()    
 pygame.quit()
 quit()
